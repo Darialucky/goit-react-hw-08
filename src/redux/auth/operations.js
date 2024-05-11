901cb7df54733668a1490828ef535601b66c3e5c
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-05-11 16:20:03 +0300

    виправлення помилок

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -59,12 +59,7 @@ export const refreshUser = createAsyncThunk(
     }
 
     try {
-      setAuthHeader(persistedToken);
-      const res = await axios.get("/users/current", {
-        headers: {
-          Authorization: `Bearer ${persistedToken}`,
-        },
-      });
+      const res = await axios.get("/users/current");
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
